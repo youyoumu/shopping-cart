8597940dd41a494af050e0ccaa8ad802f4570a29
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-30 00:26:59 +0700

    Add link to title

--- src/routes/ProductCard.jsx
+++ src/routes/ProductCard.jsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom'
+
 export default function ProductCard({
+  id,
   title,
   price,
   description,
@@ -12,11 +15,13 @@ export default function ProductCard({
         <img src={image} alt="Shoes" className="object-contain w-72 h-60" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title truncate">{title}</h2>
+        <Link to={`/products/${id}`} className="card-title truncate">
+          {title}
+        </Link>
         <div className="flex items-end">
           <p>${price}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary btn-sm">Buy Now</button>
           </div>
         </div>
       </div>
