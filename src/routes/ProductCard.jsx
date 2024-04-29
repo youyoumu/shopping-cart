78e5356b68c62ab39ed520ed1993bbe367639648
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-30 00:38:37 +0700

    Add more details to product card

--- src/routes/ProductCard.jsx
+++ src/routes/ProductCard.jsx
@@ -11,15 +11,18 @@ export default function ProductCard({
 }) {
   return (
     <div className="card w-72 h-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} alt="Shoes" className="object-contain w-72 h-60" />
+      <figure className="w-72 h-60 p-2">
+        <img src={image} alt="Shoes" className="object-contain w-full h-full" />
       </figure>
       <div className="card-body">
         <Link to={`/products/${id}`} className="card-title truncate">
           {title}
         </Link>
-        <div className="flex items-end">
-          <p>${price}</p>
+        <div className="flex items-end justify-between">
+          <div>
+            <p className="text-lg font-medium">${price}</p>
+            <p className="text-sm">{`★${rating.rate} | ${rating.count}`}</p>
+          </div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary btn-sm">Buy Now</button>
           </div>
