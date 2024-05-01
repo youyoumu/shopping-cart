f720a23fbba59adc52008c817067cd75e2d2ccc8
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-01 21:51:17 +0700

    Remove profile

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -86,37 +86,6 @@ export default function Index() {
                 </div>
               </div>
             </div>
-            <div className="dropdown dropdown-end text-base-content">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-              >
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
