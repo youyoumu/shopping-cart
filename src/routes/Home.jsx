efcb1e7c9cfbf53cb5f447d5ab748270aa9e4288
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-01 14:51:35 +0700

    Add title to home screen

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="flex justify-center flex-col items-center mt-12">
+      <h1 className="text-6xl font-bold">SHOPPING CART</h1>
+      <p className="text-2xl">a mock e-commerce website</p>
+    </div>
   )
 }
