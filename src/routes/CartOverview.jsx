6945ba60f8a8e7c6742aa527ba95d4f4fb1d329d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-01 16:29:42 +0700

    Display total price

--- src/routes/CartOverview.jsx
+++ src/routes/CartOverview.jsx
@@ -34,10 +34,21 @@ export default function CartOverview() {
     />
   ))
 
+  const totalPrice = () => {
+    let totalPrice = 0
+    productsData.forEach((product) => {
+      totalPrice += product.price * cart[product.id].quantity
+    })
+    return totalPrice
+  }
+
   return (
     <div className="max-w-2xl mx-auto">
       <h1 className="text-4xl font-bold text-center mt-4">Cart Overview</h1>
       <div className="mt-8 grid gap-4 mx-2">{CartProducts}</div>
+      <div className="text-3xl font-bold text-end mt-8">
+        Total: ${totalPrice()}
+      </div>
     </div>
   )
 }
