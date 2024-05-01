8d0beb0ef60214b8f763fea10ea1797422447db6
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-01 16:22:47 +0700

    Leverage fetching data to cart overview

--- src/routes/CartOverview.jsx
+++ src/routes/CartOverview.jsx
@@ -1,11 +1,37 @@
 import CartProduct from './CartProduct'
 import { useOutletContext } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function CartOverview() {
   const [cart, setCart] = useOutletContext()
+  const [productsData, setProductsData] = useState([]) // [productData]
 
-  const CartProducts = Object.values(cart).map((product) => (
-    <CartProduct key={product.id} product={product} />
+  useEffect(() => {
+    const fetches = []
+    Object.values(cart).forEach((product) => {
+      const getProductData = async function () {
+        const res = await fetch(
+          `https://fakestoreapi.com/products/${product.id}`
+        )
+        const data = await res.json()
+        return data
+      }
+      fetches.push(getProductData())
+    })
+
+    Promise.all(fetches).then((data) => {
+      setProductsData(data)
+    })
+  }, [cart])
+
+  if (productsData.length === 0) return <h1>Loading...</h1>
+
+  const CartProducts = productsData.map((product) => (
+    <CartProduct
+      key={product.id}
+      product={product}
+      quantity={cart[product.id].quantity}
+    />
   ))
 
   return (
--- src/routes/CartProduct.jsx
+++ src/routes/CartProduct.jsx
@@ -1,30 +1,14 @@
-import { useState, useEffect } from 'react'
-
-export default function CartProduct({ product }) {
-  const [productData, setProductData] = useState()
-
-  useEffect(() => {
-    async function getProductData() {
-      const res = await fetch(`https://fakestoreapi.com/products/${product.id}`)
-      const data = await res.json()
-      setProductData(data)
-    }
-    getProductData()
-  }, [product])
-
-  if (!productData) {
-    return <h1>Loading...</h1>
-  }
+export default function CartProduct({ product, quantity }) {
   return (
     <div>
       <div className="card card-side bg-base-100 shadow-xl">
         <figure className="min-w-20 max-w-24 p-2">
-          <img src={productData.image} alt={productData.title} />
+          <img src={product.image} alt={product.title} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{productData.title}</h2>
+          <h2 className="card-title">{product.title}</h2>
           <div>
-            <p className="text-lg font-medium">${productData.price}</p>
+            <p className="text-lg font-medium">${product.price}</p>
           </div>
           <div className="flex justify-between items-center">
             <div className="join mr-4">
@@ -32,7 +16,7 @@ export default function CartProduct({ product }) {
                 -
               </button>
               <div className="join-item min-w-8 border-solid border-slate-300 border flex justify-center items-center">
-                {product.quantity}
+                {quantity}
               </div>
               <button
                 id="quantity"
@@ -42,7 +26,7 @@ export default function CartProduct({ product }) {
               </button>
             </div>
             <div className="text-lg font-semibold">{`$${
-              product.quantity * productData.price
+              quantity * product.price
             }`}</div>
           </div>
           <div className="card-actions justify-end"></div>
